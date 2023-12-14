866faf000809b950aaa01133ca80d96b6d8f3432
nanouille <73785178+nanouille@users.noreply.github.com>
2023-12-14 17:17:16 +0100

    #1 Adding default web service

---
+++ src/main/java/fr/efrei/server/web/rest/StudentResource.java
@@ -0,0 +1,16 @@
+package fr.efrei.server.web.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class StudentResource {
+
+    @GetMapping ("/student")
+    public String test () {
+        return "Hello World!";
+    }
+
+}
