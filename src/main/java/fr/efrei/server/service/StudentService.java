c4e17e2efd59dd2c6cd3a41c6550acc2f737a461
nanouille <73785178+nanouille@users.noreply.github.com>
2023-12-15 13:58:37 +0100

    feature/3-create-a-student-entity
    data and schema of the database and class to call it using localhost:8080/api/students

---
+++ src/main/java/fr/efrei/server/service/StudentService.java
@@ -0,0 +1,23 @@
+package fr.efrei.server.service;
+
+import fr.efrei.server.domain.Student;
+import fr.efrei.server.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+-- start anew each time--
+DELETE FROM student;
+INSERT INTO student (id, name, age) VALUES (01, 'Elena', 22);
